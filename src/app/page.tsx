194d8ffa99dce774844ce1dc29e7f7d6e1617624
jpmoregain-eth <moregainjp@gmail.com>
2026-03-29 18:33:55 +0800

    Fix: pass image prop to ProjectCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,6 +77,7 @@ export default function Home() {
                   tags={project.tags}
                   link={project.link}
                   slug={project.slug}
+                  image={project.image}
                 />
               </div>
             ))}
